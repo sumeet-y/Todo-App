f464a6f77fcf54c84614744ffc889c217f55a9f6
sumeet-y <sumitpyadav74@gmail.com>
2024-07-29 01:10:55 +0530

    Added checkbox funcationality and Line through over text

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,33 +7,33 @@ const Todo = () => {
 
   const [todo, setTodo] = useState([]);
   const inputRef = useRef(null);
+
   function createTodo() {
-    e.preventDefault();
-    setTodo([...todo, { text: inputRef.current.value, no: Math.random() * 100 }]);
+    setTodo([...todo, { text: inputRef.current.value, no: Math.random() * 100}]);
     inputRef.current.value = '';
   }
 
-  useEffect(() => {
-    console.log(todo)
-  }, [todo])
+  // useEffect(() => {
+  //   console.log(todo)
+  // }, [todo])
 
   function deleteTodo(no) {
 
-    console.log("delete item no", no);
+    // console.log("delete item no", no);
     const data = todo.filter((item) => {
       return item.no != no
     })
     setTodo(data);
-    console.log("data is", data);
+    // console.log("data is", data);
   }
   return (
 
     <div className="todo-container">
       <div className="todo-box">
           <input type="text" ref={inputRef} placeholder="Create New Todo" className="input-text" />
-          <button onClick={createTodo} type="submit" className="btn" >Add</button>a
+          <button onClick={createTodo} type="submit" className="btn" >Add</button>
         {todo.map((item) => {
-          return <Todos key={item.no} no={item.no} todoText={item.text} delFun={deleteTodo} />
+          return <Todos key={item.no}  no={item.no} todoText={item.text} delFun={deleteTodo}  />
         })}      
       </div>
     </div>
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { TbCheckbox, TbDog } from "react-icons/tb";
 import "./Todos.css"
+import { FaS } from 'react-icons/fa6';
+
 const Todos = ({no,todoText,delFun}) => {
-  
+
+  const[ isChecked , setIsChecked ] = useState(false);
+  const[ style , setStyle ] = useState("none");
+
+  function toggleTodo(){
+    if(!isChecked){
+      setIsChecked(true);
+      setStyle("line-through");
+    }else{
+      setIsChecked(false);
+      setStyle("none");
+    }
+  }
+
   return (
-    <p className="todo-list" key={no}>{todoText} <RiDeleteBin6Line  onClick={()=>delFun(no)} className="delete-icon" /></p>
+    <div className="todo-list" key={no} onClick={toggleTodo} > 
+    <div className="check-box" > 
+      { isChecked ? <TbCheckbox className='pointer'/> : <MdCheckBoxOutlineBlank className='pointer'/> }
+    </div>
+      <p className="text" style={{textDecoration:style}}> {todoText}</p> 
+      <RiDeleteBin6Line  onClick={()=>delFun(no)} className="delete-icon" /> 
+    </div>
   )
 }
 
